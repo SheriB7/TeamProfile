e66b73c2404c8c9081fa302beb56db711beab08f
Sheri Brown <sherib70@gmail.com>
2022-06-02 20:12:49 -0400

    adding questions

--- index.js
+++ index.js
@@ -1,49 +1,119 @@
-const fs = require("fs")
-const inquirer = require("inquirer")
-const Employee = require(" ");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const fs = require("fs");
+const inquirer = require("inquirer");
+// const Employee = require(" ");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-{
-    type: 'input',
-    message: 'Manager',
-    name: 'name',
-},
+function roleSelection() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Select Role",
+        name: "Role",
+        choices: ["Engineer", "Intern", "Build Template"],
+        //   validate: (value) => {
+        //     if (value) {
+        //       return "i need a value to continue";
+        //     }
+        //   },
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+    });
+}
 
-{
-    type: 'list',
-    message: 'Select Role',
-        choices: ["Engineer", "Intern", "Manager"],
-    validate: (value=>{ if (value){return 'i need a value to continue'}
-    })
-},
+function employeeInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter Employee Name",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Enter Employee ID",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter Employee's Email",
+        name: "email",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+    });
+}
 
+function managerInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter Office Number",
+        name: "officeNumber",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+function engineerInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter GitHub Username",
+        name: "github",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+function internInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter School Name",
+        name: "school",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+    });
+}
 //----------MANAGER
-// enter the team manager’s 
-// name, 
-// employee ID, 
-// email address, and 
-// office number 
+// enter the team manager’s
+// name,
+// employee ID,
+// email address,
+// office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 
 //----------ENGINEER
 // WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s 
+// THEN I am prompted to enter the engineer’s
 // name,
-//  ID, 
-//  email, and 
-//  GitHub username, and 
+//  ID,
+//  email,
+//  GitHub username, and
 //  I am taken back to the menu
-
 
 //---------INTERN
 // WHEN I select the intern option
-// THEN I am prompted to enter the intern’s 
-// name, 
-// ID, 
-// email, and 
-// school, and 
+// THEN I am prompted to enter the intern’s
+// name,
+// ID,
+// email,
+// school, and
 // I am taken back to the menu
 
 // WHEN I decide to finish building my team
